6d72b0f40250121f62b44cc5636f974a1eaba6e0
poli <xyz@gmail.com>
2024-04-10 09:44:37 +0530

    Convert string to obj

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+const person = {
+name: "Polinaidu",
+Age: 21
+}
+// const result = JSON.stringify(person);
+const result = String(person);
+console.log(person["Age"])
+console.log(result);
+console.log(typeof result);
